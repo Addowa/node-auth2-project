62c873f92da8c32b694ce2cb09fc647fbeaafda9
Addow Abdi <adowabdi007@icloud.com>
2025-05-05 05:50:18 -0500

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,42 +1,55 @@
 const router = require("express").Router();
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
 const { JWT_SECRET } = require("../secrets"); // use this secret!
+const bcrypt = require('bcryptjs');
+const Users = require('../users/users-model');
 
-router.post("/register", validateRoleName, (req, res, next) => {
-  /**
-    [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
-
-    response:
-    status 201
-    {
-      "user"_id: 3,
-      "username": "anna",
-      "role_name": "angel"
-    }
-   */
+router.post("/register", validateRoleName, async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const hash = bcrypt.hashSync(password, 8); 
+    const newUser = await Users.add({
+      username,
+      password: hash,
+      role_name: req.role_name, 
+    });
+    res.status(201).json(newUser);
+  } catch (err) {
+    next(err);
+  }
 });
 
+const jwt = require('jsonwebtoken');
 
-router.post("/login", checkUsernameExists, (req, res, next) => {
-  /**
-    [POST] /api/auth/login { "username": "sue", "password": "1234" }
+function buildToken(user) {
+  const payload = {
+    subject: user.user_id,
+    username: user.username,
+    role_name: user.role_name
+  };
+  const options = {
+    expiresIn: '1d'
+  };
+  return jwt.sign(payload, JWT_SECRET, options);
+}
 
-    response:
-    status 200
-    {
-      "message": "sue is back!",
-      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ETC.ETC"
+router.post("/login", checkUsernameExists, async (req, res, next) => {
+  try {
+    const { password } = req.body;
+    const user = req.userFromDb;
+
+    if (bcrypt.compareSync(password, user.password)) {
+      const token = buildToken(user);
+      res.status(200).json({
+        message: `${user.username} is back!`,
+        token
+      });
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
     }
-
-    The token must expire in one day, and must provide the following information
-    in its payload:
-
-    {
-      "subject"  : 1       // the user_id of the authenticated user
-      "username" : "bob"   // the username of the authenticated user
-      "role_name": "admin" // the role of the authenticated user
-    }
-   */
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
